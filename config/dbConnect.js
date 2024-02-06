abe67d4177a8f68b9095183c8fbd36de52a6fbc6
qayyuma686 <abdulqayyum@hajexbolt.com>
2024-02-06 18:21:55 +0500

    remove bud in logout button

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,19 +1,3 @@
-// import mongoose from "mongoose";
-// const MONGO_URL = process.env.DATABASE_URI;
-
-// if (!MONGO_URL) {
-//   throw new Error(
-//     "Please define the MONGO_URL environment variable inside .env.local"
-//   );
-// }
-// console.log(MONGO_URL);
-// async function dbConnect() {
-//   const con = await mongoose.connect(MONGO_URL);
-//   return con;
-// }
-
-// export default dbConnect;
-
 import mongoose from "mongoose";
 const MONGO_URL = process.env.DATABASE_URI;
 
@@ -31,7 +15,7 @@ async function dbConnect() {
   if (mongoose.connection.readyState >= 1) {
     return;
   }
-
+  console.log("connection established");
   return mongoose.connect(MONGO_URL, {
     // Additional options can be specified here
   });
